29ce2ae8fbc5b9f27d5ead8625b0efed2f7a78df
francesco marinelli <francescomarinelli04@gmail.com>
2020-11-09 08:49:22 +0100

    Programma che ci permette di calcolare il numero binario

--- ConversioneNumero/Program.cs
+++ ConversioneNumero/Program.cs
@@ -6,7 +6,18 @@ namespace ConversioneNumero
     {
         static void Main(string[] args)
         {
+          
+           int  base2, numero;
+           
             
+            Console.Write("iserisci base in cui convertire");
+            base2 = int.Parse(Console.ReadLine());
+            Console.Write("inserisci in numero da convertire:");
+            numero = int.Parse(Console.ReadLine());
+            string risultato = Convert.ToString(numero, base2);
+            Console.WriteLine($"il numero convertito è {risultato}");
+
+
         }
     }
 }
